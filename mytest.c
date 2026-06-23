2fbdbfb2ca453a22b4e96d1110a7b4508c893041
abdojat <3oooo3.2003@gmail.com>
2026-06-23 20:08:52 +0300

    PART D - 4 : Test welcoming fork behavior

--- mytest.c
+++ mytest.c
@@ -1,8 +1,57 @@
-#include "types.h"      
+#include "types.h"
+#include "stat.h"
 #include "user.h"
 
-int main(int argc, char *argv[]) 
-{ 
-    printf(1, "Hey, I am trying xv6\n"); 
+static void
+welcome_child(void)
+{
+  printf(1, "mytest: welcome child entered\n");
+  if(welcomeDone() < 0){
+    printf(1, "mytest: welcomeDone failed in child\n");
     exit();
+  }
+
+  printf(1, "mytest: ERROR returned from welcomeDone\n");
+  exit();
+}
+
+int
+main(int argc, char *argv[])
+{
+  int pid;
+
+  if(welcomeDone() != -1){
+    printf(1, "mytest: welcomeDone should fail before registration\n");
+    exit();
+  }
+
+  pid = fork();
+  if(pid < 0){
+    printf(1, "mytest: fork before registration failed\n");
+    exit();
+  }
+  if(pid == 0){
+    printf(1, "mytest: first child ran normally\n");
+    exit();
+  }
+  wait();
+
+  if(welcomeFunction((void *)welcome_child) < 0){
+    printf(1, "mytest: welcomeFunction registration failed\n");
+    exit();
+  }
+
+  pid = fork();
+  if(pid < 0){
+    printf(1, "mytest: fork after registration failed\n");
+    exit();
+  }
+  if(pid == 0){
+    printf(1, "mytest: second child resumed after welcomeDone\n");
+    exit();
+  }
+  wait();
+
+  printf(1, "mytest passed\n");
+  exit();
 }
